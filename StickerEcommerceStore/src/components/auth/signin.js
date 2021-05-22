3b853d902e1e8c51b124c18dfc909a11c9892eda
JellyHead <JellyHeadM@outlook.com>
2021-05-22 13:20:59 -0400

    added singin and signup components, along with their style sheets, and added Router, Swtich, and Route to the bootstrap.js file for additional functionality.

---
+++ StickerEcommerceStore/src/components/auth/signin.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SignIn extends React.Component{
+    render(){
+        return(
+            <div className='sign-in'>
+                sign in
+            </div>
+        )
+    }
+}
+
+export default SignIn;
---
+++ StickerEcommerceStore/src/components/auth/signup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SignUp extends React.Component {
+    render(){
+        return(
+            <div className='sign-up'>
+                sign up
+            </div>
+        )
+    }
+}
+
+export default SignUp;
