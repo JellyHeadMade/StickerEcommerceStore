b0594bf8b3caf8fa383f0ed387f2e2b98f013474
JellyHead <JellyHeadM@outlook.com>
2021-05-29 12:52:54 -0400

    built the details component for quicklinks

--- StickerEcommerceStore/src/components/auth/signinForm.js
+++ StickerEcommerceStore/src/components/auth/signinForm.js
@@ -1,12 +1,32 @@
 import React from 'react';
 
 import { reduxForm, Field } from 'redux-form';
+import Details from '../details';
 
 import { FormField, FormButton } from '../formFields';
+
+import history from '../../history';
 
 class SignInForm extends React.Component {
     render() {
         const { className, handleSubmit } = this.props;
+        const links = [
+        {
+            _id: 0,
+            title: 'Not Regiestered? Create and account here',
+            onClick: () => history.push('/signup')
+        },
+        {
+            _id: 1,
+            title: 'Forgot accoutn email?',
+            onClick: () => console.log('forgot email')
+        },
+        {
+            _id: 2,
+            title: 'Forgot Password?',
+            onClick: () => console.log('forgot password')
+        }
+    ]
         return ( 
             <form onSubmit={handleSubmit} className={`${className} sign-in-form`}>
                 <Field className='sign-in-form__email' 
@@ -22,12 +42,14 @@ class SignInForm extends React.Component {
                 title='Password'
                 component={FormField} />
                 <div className='sign-in-form__line'></div>
-                <Field className='sing-in-form__login'
+                <Field className='sign-in-form__login'
                 onClick={() => console.log('trying to submint')}
                 type='submit'
                 title='Login'
                 name='Login'
                 component={FormButton}/>
+
+                <Details className='sign-in-form__details' title='QuickLinks' links={links}/>
             </form>
         )
     }
