f22c3cbac3022817da8418fdab3135f73148e35d
JellyHead <JellyHeadM@outlook.com>
2021-06-01 18:24:37 -0400

    built headernavbar reducer

---
+++ StickerEcommerceStore/src/reducers/headerNavBar.js
@@ -0,0 +1,25 @@
+import {
+    SET_HEADER_LINKS,
+    SET_NAVBAR_LINKS
+} from '../actions/types'
+
+const INITAL_STATE = {
+    headerLinks: [],
+    navbarLinks: []
+}
+
+export default function(state = INITAL_STATE, action) {
+    switch (action.type) {
+        case SET_HEADER_LINKS: 
+            return {
+                ...state,
+                headerLinks: action.payload
+            }
+        case SET_NAVBAR_LINKS: 
+            return {
+                ...state,
+                navbarLInks: action.payload
+            }
+        default: return state
+    } 
+}
