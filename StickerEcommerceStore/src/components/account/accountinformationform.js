753e4304cacad5d29a9251154848bc6325c317da
JellyHead <JellyHeadM@outlook.com>
2021-06-08 22:14:38 -0400

    built account information grid and placed majority of fields on form grid

--- StickerEcommerceStore/src/components/account/accountinformationform.js
+++ StickerEcommerceStore/src/components/account/accountinformationform.js
@@ -11,26 +11,60 @@ class AccountInformationForm extends React.Component {
         const { className, handleSubmit } = this.props;
     
         return ( 
-            <form onSubmit={handleSubmit} className={`${className} sign-in-form`}>
-                <Field className='sign-in-form__email' 
+            <form onSubmit={handleSubmit} className={`${className} account-information-form`}>
+                <Field className='account-information-form__name' 
+                type='name'
+                name='name'
+                placeholder='Name'
+                title='Name'
+                component={FormField} />
+                <Field className='account-information-form__email' 
                 type='email'
                 name='email'
                 placeholder='Email'
                 title='Email'
                 component={FormField} />
-                <Field className='sign-in-form__password' 
+
+                <Field className='account-information-form__street-address' 
+                type='address'
+                name='address'
+                placeholder='Street address'
+                title='Street address'
+                component={FormField} />
+                <Field className='account-information-form__city' 
+                type='city'
+                name='city'
+                placeholder='City'
+                title='City'
+                component={FormField} />
+
+                <Field className='account-information-form__state' 
+                type='state'
+                name='state'
+                placeholder='State'
+                title='State'
+                component={FormField} />
+                <Field className='account-information-form__zipcode' 
+                type='zipcode'
+                name='zipcode'
+                placeholder='Zipcode'
+                title='Zipcode'
+                component={FormField} />
+
+
+                {/* <Field className='account-information-form__password' 
                 type='password'
                 name='password'
                 placeholder='Password'
                 title='Password'
                 component={FormField} />
-                <div className='sign-in-form__line'></div>
-                <Field className='sign-in-form__login'
+                <div className='account-information-form__line'></div>
+                <Field className='account-information-form__login'
                 onClick={() => history.push('/account')}
                 type='submit'
                 title='Login'
                 name='Login'
-                component={FormButton}/>
+                component={FormButton}/> */}
             </form>
         )
     }
