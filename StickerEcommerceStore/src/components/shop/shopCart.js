2b8b7fcb4f070cea082eb984b9d25741b9643193
JellyHead <JellyHeadM@outlook.com>
2021-06-28 20:09:51 -0400

    structured cart content products component and styled title

--- StickerEcommerceStore/src/components/shop/shopCart.js
+++ StickerEcommerceStore/src/components/shop/shopCart.js
@@ -12,10 +12,19 @@ function CartButton({className, icon}) {
     )
 }
 
-function CartContent({className}) {
+function CartContent({className, products}) {
+    let count = products.length;
     return(
-        <div className={`${className} card-content`} >
+        <div className={`${className} cart-content`} >
+            <div className='cart-content__title'>
+                Cart ({count})
+            </div>
+            <div className='cart-content__products'>
 
+            </div>
+            <div className='cart-content__footer'>
+
+            </div>
         </div>
     )
 }
@@ -26,7 +35,7 @@ class ShopCart extends React.Component {
         return (
             <div className={`${className} shop-cart`}>
                 <CartButton className='shop-cart__toggle' icon='faTimes' />
-                <CartContent className='shop-cart__content' />
+                <CartContent className='shop-cart__content' products={[4]}/>
             </div>
         )
     }
