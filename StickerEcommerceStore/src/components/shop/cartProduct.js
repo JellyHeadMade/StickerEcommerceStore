5f5f21b0ca77df6cc5077d31da28c482f2318f6f
JellyHead <JellyHeadM@outlook.com>
2021-06-28 21:56:07 -0400

    mapped cartproducts into cart product component instances

--- StickerEcommerceStore/src/components/shop/cartProduct.js
+++ StickerEcommerceStore/src/components/shop/cartProduct.js
@@ -4,9 +4,10 @@ import Quantity from '../quantity';
 
 class CartProduct extends React.Component {
     render() {
-        const { className, title, quantity, price } = this.props;
+        const {  product, quantity } = this.props;
+        const {  title,  price } = product;
         return(
-            <div className={`${className} cart-product`}>
+            <div className={` cart-product`}>
                 <img className='cart-product__image' src='http://via.placeholder.com/130x130' />
                 <div className='cart-product__title'>{title}</div>
                 <Quantity className='cart-product__quantity' quantity={quantity}/>
