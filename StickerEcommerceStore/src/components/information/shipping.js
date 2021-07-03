f34aff5fcf0e25094166550fab907be79bef5f35
JellyHead <JellyHeadM@outlook.com>
2021-07-02 20:20:50 -0400

    built the information routes and container components payment and shipping js

---
+++ StickerEcommerceStore/src/components/information/shipping.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import PageTitle from '../pageTitle';
+
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
+class Shipping extends React.Component {
+
+    componentDidMount() {
+        this.props.setHeaderLinks([]);
+        this.props.setNavbarLinks([]);
+    }
+
+    onSubmit = (Fields) => {
+        console.log(Fields);
+        this.props.history.push('/account');
+    }
+
+    render() {
+        return(
+            <div className='sign-in'>
+                <PageTitle className='sign-in__page-title' title='Shipping Address' />
+                {/* <ShippingForm onSubmit={this.onSubmit} className='sign-in__form' /> */}
+            </div>
+        )
+    }
+}
+
+Shipping = connect(null, actions)(Shipping);
+
+export default Shipping;
